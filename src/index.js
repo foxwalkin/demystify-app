33d161c672f412cf433ceb044d298701ce071c84
Alexander Emile <foxwalkin@gmail.com>
2018-09-15 17:41:33 -0400

    implemented react-redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import rootReducer from './reducers';
+
+// CSS
 import './index.css';
-import App from './App';
+
+// Service Worker
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
+
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+// import various reducers here
+
+export default combineReducers({
+	// put various reducers here
+});
